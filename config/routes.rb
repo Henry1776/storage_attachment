8242fe72661260b27f84da74875438b8d0e84905
henry <henryartavia767@hotmail.com>
2021-05-06 17:45:38 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :profiles
+  resources :products do 
+    member do
+    delete 'delete_image/:image_id', to: 'products#delete_image', as: 'delete_image'
+    end
+  end
+end
